8b7dd179d845b9bfeda24e6821863077341d55b0
Scott Myron <samyron@gmail.com>
2025-04-24 22:34:48 -0500

    Fix an issue where characters were being escaped when they shouldn't.

--- ext/json/ext/generator/generator.c
+++ ext/json/ext/generator/generator.c
@@ -188,11 +188,15 @@ static inline FORCE_INLINE void escape_UTF8_char_basic(search_state *search)
         case '\r': fbuffer_append(search->buffer, "\\r", 2);  break;
         case '\t': fbuffer_append(search->buffer, "\\t", 2);  break;
         default: {
-            const char *hexdig = "0123456789abcdef";
-            char scratch[6] = { '\\', 'u', '0', '0', 0, 0 };
-            scratch[4] = hexdig[(ch >> 4) & 0xf];
-            scratch[5] = hexdig[ch & 0xf];
-            fbuffer_append(search->buffer, scratch, 6);
+            if (ch < 0x32) {
+                const char *hexdig = "0123456789abcdef";
+                char scratch[6] = { '\\', 'u', '0', '0', 0, 0 };
+                scratch[4] = hexdig[(ch >> 4) & 0xf];
+                scratch[5] = hexdig[ch & 0xf];
+                fbuffer_append(search->buffer, scratch, 6);
+            } else {
+                fbuffer_append_char(search->buffer, ch);
+            }
             break;
         }
     }
