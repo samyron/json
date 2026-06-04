216e0ffd27c80d343bfdf402e2df7a305fa23dae
Jean Boussier <jean.boussier@gmail.com>
2026-06-04 08:59:55 +0200

    Reove outdated benchmark notes
    We've been faster on all benchmarks for a while now.
    All these notes have been addressed a long time ago.

--- benchmark/encoder.rb
+++ benchmark/encoder.rb
@@ -63,37 +63,18 @@ def benchmark_encoding(benchmark_name, ruby_obj, check_expected: true, except: [
   puts
 end
 
-# NB: Notes are based on ruby 3.3.4 (2024-07-09 revision be1089c8ec) +YJIT [arm64-darwin23]
-
-# On the first two micro benchmarks, the limiting factor is the fixed cost of initializing the
-# generator state. Since `JSON.generate` now lazily allocate the `State` object we're now ~10-20% faster
-# than `Oj.dump`.
 benchmark_encoding "small mixed", [1, "string", { a: 1, b: 2 }, [3, 4, 5]]
 benchmark_encoding "small nested array", [[1,2,3,4,5]]*10
 benchmark_encoding "small hash", { "username" => "jhawthorn", "id" => 123, "event" => "wrote json serializer" }
 
-# On string encoding we're ~20% faster when dealing with mostly ASCII, but ~50% slower when dealing
-# with mostly multi-byte characters. There's likely some gains left to be had in multi-byte handling.
 benchmark_encoding "mixed utf8", ([("a" * 5000) + "€" + ("a" * 5000)] * 500)
 benchmark_encoding "mostly utf8", ([("€" * 3333)] * 500)
 
-# On these benchmarks we perform well, we're on par or a bit better.
 benchmark_encoding "integers", (1_000_000..1_001_000).to_a, except: %i(json_state)
 benchmark_encoding "activitypub.json", JSON.load_file("#{__dir__}/data/activitypub.json")
 benchmark_encoding "citm_catalog.json", JSON.load_file("#{__dir__}/data/citm_catalog.json")
 benchmark_encoding "twitter.json", JSON.load_file("#{__dir__}/data/twitter.json")
 
-# This benchmark spent the overwhelming majority of its time in `ruby_dtoa`. We rely on Ruby's implementation
-# which uses a relatively old version of dtoa.c from David M. Gay.
-# Oj in `compat` mode is ~10% slower than `json`, but in its default mode is noticeably faster here because
-# it limits the precision of floats, breaking roundtripping. That's not something we should emulate.
-#
-# Since a few years there are now much faster float to string implementations such as Ryu, Dragonbox, etc,
-# but all these are implemented in C++11 or newer, making it hard if not impossible to include them.
-# Short of a pure C99 implementation of these newer algorithms, there isn't much that can be done to match
-# Oj speed without losing precision.
 benchmark_encoding "canada.json", JSON.load_file("#{__dir__}/data/canada.json"), check_expected: false
 
-# We're about 10% faster when `to_json` calls are involved, but this wasn't particularly optimized, there might be
-# opportunities here.
 benchmark_encoding "many #to_json calls", [{object: Object.new, int: 12, float: 54.3, class: Float, time: Time.now, date: Date.today}] * 20
--- benchmark/parser.rb
+++ benchmark/parser.rb
@@ -42,18 +42,14 @@ def benchmark_parsing(name, json_output)
   puts
 end
 
-# NB: Notes are based on ruby 3.3.4 (2024-07-09 revision be1089c8ec) +YJIT [arm64-darwin23]
-
 benchmark_parsing "small nested array", JSON.dump([[1,2,3,4,5]]*10)
 benchmark_parsing "small hash", JSON.dump({ "username" => "jhawthorn", "id" => 123, "event" => "wrote json serializer" })
-benchmark_parsing "test from oj", <<JSON
-{"a":"Alpha","b":true,"c":12345,"d":[true,[false,[-123456789,null],3.9676,["Something else.",false],null]],
-"e":{"zero":null,"one":1,"two":2,"three":[3],"four":[0,1,2,3,4]},"f":null,
-"h":{"a":{"b":{"c":{"d":{"e":{"f":{"g":null}}}}}}},"i":[[[[[[[null]]]]]]]}
+benchmark_parsing "test from oj", <<~'JSON'
+  {"a":"Alpha","b":true,"c":12345,"d":[true,[false,[-123456789,null],3.9676,["Something else.",false],null]],
+  "e":{"zero":null,"one":1,"two":2,"three":[3],"four":[0,1,2,3,4]},"f":null,
+  "h":{"a":{"b":{"c":{"d":{"e":{"f":{"g":null}}}}}}},"i":[[[[[[[null]]]]]]]}
 JSON
 
-# On these macro-benchmarks, we're on par with `Oj::Parser`, except `twitter.json` where we're `1.14x` faster,
-# And between 1.3x and 1.5x faster than `Oj.load`.
 benchmark_parsing "activitypub.json", File.read("#{__dir__}/data/activitypub.json")
 benchmark_parsing "twitter.json", File.read("#{__dir__}/data/twitter.json")
 benchmark_parsing "citm_catalog.json", File.read("#{__dir__}/data/citm_catalog.json")
